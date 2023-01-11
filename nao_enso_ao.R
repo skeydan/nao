dc3776a5b099efcccdbc15b115778de10236ede5
skeydan <keydana@gmx.de>
2023-01-11 22:12:24 +0100

    wip

--- nao_enso_ao.R
+++ nao_enso_ao.R
@@ -33,8 +33,13 @@ library(feasts)
 
 ########################   NAO   ########################
 
+download.file(
+  "https://crudata.uea.ac.uk/cru/data//nao/nao.dat",
+  destfile = "data/nao.dat"
+)
+
 # last valid value is 2022-9
-# for all 3, use 1950-01 to 2022-11
+# for all 3, use 1950-01 to 2022-09
 
 use_months <- seq.Date(
   # from = as.Date("1824-01-01"),
@@ -75,9 +80,10 @@ df %>% ggplot(aes(f, y)) +
   geom_line() +
   xlab("frequency (per year)") +
   ylab("magnitude") +
-  ggtitle("tbd")
+  ggtitle("Spectrum of NAO data.")
 
-strongest <- torch_topk(fft[1:(nyquist_cutoff/2)]$abs(), 9)
+strongest <- torch_topk(fft[1:(nyquist_cutoff/2)]$abs(), 6)
+strongest
 # [[1]]
 # torch_tensor
 # 102.7191
@@ -86,10 +92,7 @@ strongest <- torch_topk(fft[1:(nyquist_cutoff/2)]$abs(), 9)
 # 75.9949
 # 72.9086
 # 60.8281
-# 59.5460
-# 56.0690
-# 55.6052
-# [ CPUFloatType{9} ]
+# [ CPUFloatType{6} ]
 # 
 # [[2]]
 # torch_tensor
@@ -99,17 +102,14 @@ strongest <- torch_topk(fft[1:(nyquist_cutoff/2)]$abs(), 9)
 # 59
 # 33
 # 78
-# 164
-# 90
-# 74
-# [ CPULongType{9} ]
+# [ CPULongType{6} ]
 
 important_freqs <- frequencies[as.numeric(strongest[[2]])]
 important_freqs
-# [1] 2.0068729 1.3470790 1.9931271 0.7972509 0.4398625 1.0584192 2.2405498 1.2233677 1.0034364
+# [1] 2.0068729 1.3470790 1.9931271 0.7972509 0.4398625 1.0584192
 num_observations_in_season <- 12/important_freqs  
 num_observations_in_season
-# [1]  5.979452  8.908163  6.020690 15.051724 27.281250 11.337662  5.355828 9.808989 11.958904
+# [1]  5.979452  8.908163  6.020690 15.051724 27.281250 11.337662
 nao %>%
   model(STL(nao ~ season(period = 6) + season(period = 9) + season(period = 15) + season(period = 27) + season(period = 12))) %>%
   components() %>%
@@ -129,7 +129,10 @@ nao %>% ACF(nao) %>% autoplot()
 
 
 ########################   ENSO   ########################
-
+download.file(
+  "https://bmcnoldy.rsmas.miami.edu/tropics/oni/ONI_NINO34_1854-2022.txt",
+  destfile = "ONI_NINO34_1854-2022.txt"
+)
 
 enso <- read_table("data/ONI_NINO34_1854-2022.txt", skip = 9) %>%
   mutate(x = yearmonth(as.Date(paste0(YEAR, "-", `MON/MMM`, "-01")))) %>%
@@ -194,14 +197,16 @@ enso %>% ACF(enso) %>% autoplot()
 
 ########################   AO    ########################
 
-file_length <- length(readLines("data/ao.data"))
+download.file(
+  "https://www.cpc.ncep.noaa.gov/products/precip/CWlink/daily_ao_index/monthly.ao.index.b50.current.ascii.table",
+  destfile = "data/ao.dat"
+)
+
 ao <-
   read_table(
-    "data/ao.data",
+    "data/ao.dat",
     col_names = FALSE,
-    na = "-99.99",
-    skip = 1,
-    n_max = file_length - 4
+    skip = 1
   ) %>%
   select(-X1) %>%
   as.matrix() %>% 
@@ -229,9 +234,9 @@ df %>% ggplot(aes(f, y)) +
   geom_line() +
   xlab("frequency (per year)") +
   ylab("magnitude") +
-  ggtitle("Spectrum of Niño 3.4 data")
+  ggtitle("Spectrum ofAO data")
 
-strongest <- torch_topk(fft[1:(nyquist_cutoff/2)]$abs(), 9)
+strongest <- torch_topk(fft[1:(nyquist_cutoff/2)]$abs(), 5)
 strongest
 # [[1]]
 # torch_tensor
@@ -240,11 +245,7 @@ strongest
 # 72.3889
 # 72.3759
 # 72.2528
-# 70.6427
-# 67.7424
-# 66.5320
-# 65.5934
-# [ CPUFloatType{9} ]
+# [ CPUFloatType{5} ]
 # 
 # [[2]]
 # torch_tensor
@@ -253,20 +254,16 @@ strongest
 # 130
 # 94
 # 6
-# 4
-# 57
-# 21
-# 74
-# [ CPULongType{9} ]
+# [ CPULongType{5} ]
 
 important_freqs <- frequencies[as.numeric(strongest[[2]])]
 important_freqs
-# [1] 0.01374570 0.35738832 1.77319588 1.27835052 0.06872852 0.04123711 0.76975945 0.27491409 1.00343643
+# [1] 0.01374570 0.35738832 1.77319588 1.27835052 0.06872852
 num_observations_in_season <- 12/important_freqs  
 num_observations_in_season
-# [1] 873.000000  33.576923   6.767442   9.387097 [5] 174.600000 291.000000  15.589286  43.650000 11.958904
+# [1] 873.000000  33.576923   6.767442   9.387097 174.600000 
 ao %>%
-  model(STL(ao ~ season(period = 33) + season(period = 7) + season(period = 9) + season(period = 291) + season(period = 15))) %>%
+  model(STL(ao ~ season(period = 33) + season(period = 7) + season(period = 9) + season(period = 174))) %>%
   components() %>%
   autoplot()
 
@@ -287,15 +284,13 @@ library(torchwavelets)
 ### nao ###
 
 nao_idx <- nao$nao %>% as.numeric() %>% torch_tensor()
-dt <- 1/12
-
+dt <- 1/12 # same interval as for ENSO
 wtf <- wavelet_transform(length(nao_idx), dt = dt)
 power_spectrum <- wtf$power(nao_idx)
 
-times <- lubridate::year(nao$x) + lubridate::month(nao$x)/12
-coi <- wtf$coi(times[1], times[length(nao_idx)])
+times <- lubridate::year(nao$x) + lubridate::month(nao$x)/12 # also same
+scales <- as.numeric(wtf$scales) # will be same because both series have same length
 
-scales <- as.numeric(wtf$scales)
 df <- as_tibble(as.matrix(power_spectrum$t()), .name_repair = "universal") %>%
   mutate(time = times) %>%
   pivot_longer(!time, names_to = "scale", values_to = "power") %>%
@@ -303,13 +298,13 @@ df <- as_tibble(as.matrix(power_spectrum$t()), .name_repair = "universal") %>%
                           str_remove("[\\.]{3}") %>%
                           as.numeric()])
 
+coi <- wtf$coi(times[1], times[length(nao_idx)])
 coi_df <- data.frame(x = as.numeric(coi[[1]]), y = as.numeric(coi[[2]]))
 
-labeled_scales <- c(0.25, 0.5, 1, 2, 4, 8, 16, 32, 64)
+labeled_scales <- c(0.25, 0.5, 1, 2, 4, 8, 16, 32, 64) # same since scales are same 
 labeled_frequencies <- round(as.numeric(wtf$fourier_period(labeled_scales)), 1)
 
 ggplot(df) +
-  #scale_y_continuous(trans = c("log10", "reverse")) + 
   scale_y_continuous(
     trans = scales::compose_trans(scales::log2_trans(), scales::reverse_trans()),
     breaks = c(0.25, 0.5, 1, 2, 4, 8, 16, 32, 64),
@@ -324,9 +319,8 @@ ggplot(df) +
   xlab("year") +
   geom_contour_filled(aes(time, scale, z = power), show.legend = FALSE) +
   scale_fill_viridis_d(option = "turbo") +
-  geom_ribbon(data = coi_df, aes(x = x, ymin = y, ymax = max(scales)), fill = "black", alpha = 0.3) +
+  geom_ribbon(data = coi_df, aes(x = x, ymin = y, ymax = max(scales)), fill = "black", alpha = 0.6) +
   theme(legend.position = "none")
-
 
 
 ### enso ###
@@ -375,11 +369,12 @@ ggplot(df) +
 ### ao ###
 
 ao_idx <- ao$ao %>% as.numeric() %>% torch_tensor()
-
+dt <- 1/12 # same interval as for ENSO and NAO
 wtf <- wavelet_transform(length(ao_idx), dt = dt)
 power_spectrum <- wtf$power(ao_idx)
 
-coi <- wtf$coi(times[1], times[length(ao_idx)])
+times <- lubridate::year(ao$x) + lubridate::month(ao$x)/12 # also same
+scales <- as.numeric(wtf$scales) # will be same because all series have same length
 
 df <- as_tibble(as.matrix(power_spectrum$t()), .name_repair = "universal") %>%
   mutate(time = times) %>%
@@ -388,10 +383,13 @@ df <- as_tibble(as.matrix(power_spectrum$t()), .name_repair = "universal") %>%
                           str_remove("[\\.]{3}") %>%
                           as.numeric()])
 
+coi <- wtf$coi(times[1], times[length(ao_idx)])
 coi_df <- data.frame(x = as.numeric(coi[[1]]), y = as.numeric(coi[[2]]))
 
+labeled_scales <- c(0.25, 0.5, 1, 2, 4, 8, 16, 32, 64) # same since scales are same 
+labeled_frequencies <- round(as.numeric(wtf$fourier_period(labeled_scales)), 1)
+
 ggplot(df) +
-  #scale_y_continuous(trans = c("log10", "reverse")) + 
   scale_y_continuous(
     trans = scales::compose_trans(scales::log2_trans(), scales::reverse_trans()),
     breaks = c(0.25, 0.5, 1, 2, 4, 8, 16, 32, 64),
@@ -406,6 +404,7 @@ ggplot(df) +
   xlab("year") +
   geom_contour_filled(aes(time, scale, z = power), show.legend = FALSE) +
   scale_fill_viridis_d(option = "turbo") +
-  geom_ribbon(data = coi_df, aes(x = x, ymin = y, ymax = max(scales)), fill = "black", alpha = 0.3) +
+  geom_ribbon(data = coi_df, aes(x = x, ymin = y, ymax = max(scales)), fill = "black", alpha = 0.6) +
   theme(legend.position = "none")
+
 
